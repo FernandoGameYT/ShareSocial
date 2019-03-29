dbc62512588fc754fc280c3824c9752549b0ec20
Fernando <vacatamayo321@gmail.com>
2019-03-29 17:54:45 -0600

    Init the proyect

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand font-weight-bold" href="#">
+            <img src="https://yt3.ggpht.com/-q6K9UKgIoVs/AAAAAAAAAAI/AAAAAAAAAAg/VnRohwbi_JA/s48-nd-c-c0xffffffff-rj-k-no/photo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+            Two Steps From Hell
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Home</a>
+            </div>
+        </div>
+    </nav>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
